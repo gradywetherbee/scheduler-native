bb15cac37e46337d9e0f58e142f1481ae6eee978
gradywetherbee <gradywetherbee2021@u.northwestern.edu>
2020-09-19 23:17:39 -0700

    added TermSelector & active  state

--- components/CourseList.js
+++ components/CourseList.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
 import Course from './Course';
+import TermSelector from './TermSelector';
 
-const CourseList = ({ courses }) => (
-    <ScrollView>
-        <View style={styles.courseList}>
-            {courses.map(course => <Course key={course.id} course={course} />)}
-        </View>
-    </ScrollView>
-);
+const CourseList = ({ courses }) => {
+    const termMap = { F: 'Fall', W: 'Winter', S: 'Spring' };
+    const terms = Object.values(termMap);
+
+    const getCourseTerm = course => (
+        termMap[course.id.charAt(0)]
+    );
+
+    const [selectedTerm, setSelectedTerm] = useState('Winter');
+    const termCourses = courses.filter(course => selectedTerm === getCourseTerm(course));
+
+    return (
+        <React.Fragment>
+            <TermSelector selectedTerm={selectedTerm} setSelectedTerm={setSelectedTerm} />
+            <ScrollView>
+                <View style={styles.courseList}>
+                {termCourses.map(course => <Course key={course.id} course={course} />)}
+                </View>
+            </ScrollView>
+        </React.Fragment >
+    );
+}
 
 const styles = StyleSheet.create({
     courseList: {
